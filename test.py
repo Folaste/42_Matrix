c8a8959f87dc20ff7844859f31edda9e1d40c2ab
Folaste <leblancflo@orange.fr>
2023-10-26 12:01:17 +0200

    Finished constructor and utility functions for Vector (Need to add Vector.reshape())

--- test.py
+++ test.py
@@ -4,9 +4,17 @@ from matrix import Matrix
 
 def main():
     try:
-        vect = Vector([[2, 2], ["2", 1]])
-        print(vect)
-        # vect.dim()
+        vect_c = Vector([[2.5, 2], [2, -1]])
+        vect_c.print()
+        vect_c.dim()
+        print()
+        vect = Vector([1, 98, 65, 2, 6])
+        vect.print()
+        vect.dim()
+        print()
+        test = Vector([1, "Oui", False], [])
+        test.print()
+        test.dim()
     except AssertionError as e:
         print(e)
         exit(1)
--- vector.py
+++ vector.py
@@ -1,36 +1,50 @@
 class Vector:
 
-    def __init__(self, *elements):
-        if not all(isinstance(elem, list) for elem in elements) or len(elements) != 1:
-            raise AssertionError("Constructor takes only 1 list.")
-
-        # TODO: Didn't work on complex
-        elements = elements[0]
+    def __init__(self, *elements: list):
         try:
+            if not all(isinstance(elem, list) for elem in elements) or len(elements) != 1:
+                raise AssertionError("Constructor takes only 1 list.")
+            elements = elements[0]
+
             if not all(isinstance(elem, (int, float, list))
                        for elem in elements):
-                raise AssertionError("Vector must only contain float, "
-                                     "int or complex numbers.")
+                raise AssertionError("Vector must only contain float, int or complex numbers.")
+
             if all(isinstance(elem, list) for elem in elements):
+                c_elements = []
                 for elem in elements:
                     if not all(isinstance(comp, (int, float)) for comp in elem) or len(elem) != 2:
-                        raise AssertionError("Complex number must have only 2 "
-                                             "parts and must be ints "
-                                             "or floats.")
+                        raise AssertionError("Complex number must have only 2 parts and must be ints or floats.")
+                    elem = [float(comp) for comp in elem]
+                    c_elements.append(elem)
+                self.elements = c_elements
+                self.dimension = len(c_elements)
+
             elif not all(isinstance(elem, (int, float)) for elem in elements):
-                raise AssertionError("Vector must only contain float, "
-                                     "int or complex numbers.")
-            else: # TODO : Tout mettre en float
+                raise AssertionError("Vector must only contain float, int or complex numbers.")
+
+            else:
+                elements = [float(elem) for elem in elements]
                 self.elements = elements
-                print(self.elements)
                 self.dimension = len(elements)
+
         except AssertionError as e:
             print(e)
-            exit(1)
+            raise AssertionError("Error while creating Vector")
 
     def dim(self):
         print(f"Size of the vector is : {self.dimension}")
 
     def print(self):
-        print(self.elements)
-        # TODO: Complex case
+        if isinstance(self.elements[0], list):
+            print("[", end='')
+            for i in range(self.dimension):
+                if self.elements[i][1] < 0:
+                    print(f"{self.elements[i][0]}{self.elements[i][1]}i", end='')
+                else:
+                    print(f"{self.elements[i][0]}+{self.elements[i][1]}i", end='')
+                if i != self.dimension - 1:
+                    print(", ", end='')
+            print("]")
+        else:
+            print(self.elements)
