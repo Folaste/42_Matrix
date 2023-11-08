e637c4074dd5e764e29edcd2cfeac1da9bee9b5a
Folaste <leblancflo@orange.fr>
2023-11-08 18:09:58 +0100

    End of day 8/11/23

---
+++ ex00.py
@@ -0,0 +1,40 @@
+"""
+TODO Vector:
+    Sum:
+        R + R
+        C + C
+        C + R
+    Sub :
+        R - R
+        C - C
+        C - R
+        R - C
+    Scl:
+        RS * R
+        CS * R
+        RS * C
+        CS * C
+    Exceptions:
+        - Different sizes
+"""
+from complex_builtin.matrix import Matrix as B_Matrix
+from complex_builtin.vector import Vector as B_Vector
+
+from complex_created.complex import Complex
+from complex_created.matrix import Matrix as C_Matrix
+from complex_created.vector import Vector as C_Vector
+
+
+
+
+def main_builtin():
+
+
+
+def main_created():
+
+
+if __name__ == "__main__":
+    main_builtin()
+    print()
+    main_created()
