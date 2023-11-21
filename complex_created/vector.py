7a8783f84238b726f3832d8fdf217765deddb781
Folaste <41912691+Folaste@users.noreply.github.com>
2023-11-21 17:06:25 +0100

    Finished ex06

--- complex_created/vector.py
+++ complex_created/vector.py
@@ -29,6 +29,14 @@ class Vector:
             print(e)
             raise AssertionError("Error while creating Vector")
 
+    def __getitem__(self, index):
+        try:
+            if index < self._dimension:
+                return self._elements[index]
+            raise AssertionError("Index is above dimension of the vector.")
+        except AssertionError as e:
+            print(e)
+
     def __str__(self):
         max_width = max(len(str(element)) for element in self._elements)
         string = ""
---
+++ ex06.py
@@ -0,0 +1,53 @@
+"""
+    Exercise 06: Cross Product
+"""
+
+from complex_created.complex import Complex
+from complex_created.vector import Vector
+
+
+def cross_product(u: Vector, v: Vector) -> Vector:
+    try:
+        if not isinstance(u, Vector) or not isinstance(v, Vector):
+            raise TypeError()
+        if not u.dim() == v.dim() == 3:
+            raise ValueError()
+        else:
+            return Vector([u[1] * v[2] - u[2] * v[1],
+                           u[2] * v[0] - u[0] * v[2],
+                           u[0] * v[1] - u[1] * v[0]])
+
+    except TypeError:
+        print("u and v must be vectors.")
+    except ValueError:
+        print("u and v must have 3 dimensions.")
+
+
+def main():
+    u = Vector([0, 0, 1])
+    v = Vector([1, 0, 0])
+    print(cross_product(u, v))
+
+    u = Vector([1, 2, 3])
+    v = Vector([4, 5, 6])
+    print(cross_product(u, v))
+
+    u = Vector([4, 2, -3])
+    v = Vector([-2, -5, 16])
+    print(cross_product(u, v))
+
+    u = Vector([Complex(1, 0), Complex(2, 1), Complex(1, 2)])
+    v = Vector([4, 7, 12])
+    print(cross_product(u, v))
+
+    u = Vector([1, 2])
+    v = Vector([3, 4, 5])
+    print(cross_product(u, v))
+
+    u = "Test"
+    v = Vector([4, 7, 12])
+    print(cross_product(u, v))
+
+
+if __name__ == "__main__":
+    main()
