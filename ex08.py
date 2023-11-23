d3af99536bbeca1e6e07ceaacf8e5e8bffb9c2e4
Folaste <41912691+Folaste@users.noreply.github.com>
2023-11-23 12:16:32 +0100

    Finished ex08

--- ex08.py
+++ ex08.py
@@ -5,6 +5,7 @@
 from complex_created.complex import Complex
 from complex_created.matrix import Matrix
 
+
 def main():
     a = Matrix([[1, 2], [3, 4]])
     print(a)
@@ -15,6 +16,14 @@ def main():
     print(a.trace(), end="\n\n")
 
     a = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    print(a)
+    print(a.trace(), end="\n\n")
+
+    a = Matrix([[2, -5, 0], [4, 3, 7], [-2, 3, 4]])
+    print(a)
+    print(a.trace(), end="\n\n")
+
+    a = Matrix([[-2, -8, 4], [1, -23, 4], [0, 6, 4]])
     print(a)
     print(a.trace(), end="\n\n")
 
